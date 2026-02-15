611012b55c00ab46e6e15f5bbdcf661c1192d6e9
Yash Batham <yashombatham@gmail.com>
2026-02-15 20:44:16 +0530

    analytics import fix

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
